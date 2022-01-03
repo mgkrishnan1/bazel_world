5f47d62ae72093322e8b7f7afbd3160a435a9ac4
mgkrishnan1 <muralikrishna1997@gmail.com>
2022-01-03 12:18:38 +0530

    Adding setup files

---
+++ bazel_world/__init__.py
@@ -0,0 +1,2 @@
+name = "bazel_world"
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="bazel_world",
+    version="0.0.1",
+    author="Cedd Burge",
+    author_email="ceddlyburge@gmail.com",
+    description="A function that returns 'world'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mgkrishnan1/bazel_world",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
